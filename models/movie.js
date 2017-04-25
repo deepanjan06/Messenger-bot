6e68d43da81ef4cbea6aec53f5f145ef33a96898
deepanjan06 <deepanjan06@users.noreply.github.com>
2017-04-25 11:53:58 +0100

    Added awards

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+var movieSchema = mongoose.movieSchema;
 
-var MovieSchema = new Schema({
+var movie_db = new Schema({
     user_id: {type: String},
     title: {type: String},
     plot: {type: String},
@@ -10,7 +10,10 @@ var MovieSchema = new Schema({
     director: {type: String},
     cast: {type: String},
     rating: {type: String},
+    writer: {type: String},
+    genre: {type: String},
+    awards: {type: String},
     poster_url: {type: String}
 });
 
-module.exports = mongoose.model("Movie", MovieSchema);
+module.exports = mongoose.model("Movie", movie_db);
